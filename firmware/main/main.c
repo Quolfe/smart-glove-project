ae83703e049d8f8d15eb0e8deac8dc8c8da4d6b3
Ethan <edjordan20@outlook.com>
2026-04-04 23:50:25 -0500

    Added task to display the finger input status

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,5 +1,4 @@
 #include "freertos/idf_additions.h"
-#include "portmacro.h"
 #include "sh1106.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -41,6 +40,105 @@ void update_display(void *arg) {
     }
 }
 
+void draw_status(void *arg) {
+}
+
+void draw_finger_status(void *arg) {
+    uint8_t gap = 8;
+    uint8_t bits_1[] = {
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 3, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 3, 3, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    uint8_t bits_2[] = {
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 3, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 3, 2, 2, 2, 2, 1,
+        1, 2, 2, 3, 3, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    uint8_t bits_3[] = {
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 2, 2, 3, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 3, 3, 2, 2, 2, 1,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    uint8_t bits_4[] = {
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 2, 2, 3, 2, 3, 2, 2, 1,
+        1, 2, 2, 3, 2, 3, 2, 2, 1,
+        1, 2, 2, 3, 3, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 3, 2, 2, 1,
+        1, 2, 2, 2, 2, 2, 2, 2, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    bitmap_t bmap_1 = {
+        .data = bits_1,
+        .x_size = 9,
+        .y_size = 10,
+        .x = 128 / 2 - bmap_1.x_size * 2 - gap * 3 / 2,
+        .y = 64 - bmap_1.y_size,
+    };
+    bitmap_t bmap_2 = {
+        .data = bits_2,
+        .x_size = 9,
+        .y_size = 10,
+        .x = 128 / 2 - bmap_2.x_size - gap / 2,
+        .y = 64 - bmap_2.y_size,
+    };
+    bitmap_t bmap_3 = {
+        .data = bits_3,
+        .x_size = 9,
+        .y_size = 10,
+        .x = 128 / 2 + gap / 2,
+        .y = 64 - bmap_3.y_size,
+    };
+    bitmap_t bmap_4 = {
+        .data = bits_4,
+        .x_size = 9,
+        .y_size = 10,
+        .x = 128 / 2 + bmap_4.x_size + gap * 3 / 2,
+        .y = 64 - bmap_4.y_size,
+    };
+    bitmap_t bmaps[] = { bmap_1, bmap_2, bmap_3, bmap_4 };
+    bool on = false;
+    while (1) {
+        for (int i = 0; i < sizeof(bmaps) / sizeof(*bmaps); i++) {
+            bitmap_t bmap = bmaps[i];
+            for (uint8_t x = 0; x < bmap.x_size; x++) {
+                for (uint8_t y = 0; y < bmap.y_size; y++) {
+                    uint8_t data_at = bmap.data[y * bmap.x_size + x];
+                    bool set_pixel = (data_at == 1) || (data_at == 2 && on) || (data_at == 3 && !on);
+                    sh1106_draw_pixel(display, x + bmap.x, y + bmap.y, set_pixel);
+                }
+            }
+        }
+        on ^= true;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main(void) {
     ESP_ERROR_CHECK(init_i2c());
 
@@ -49,5 +147,6 @@ void app_main(void) {
     sh1106_config_t config = sh1106_default_config();
     ESP_ERROR_CHECK(sh1106_init(config, i2c_bus_handle, display));
 
-    xTaskCreate(update_display, "update sh1106", 2048, NULL, 5, NULL);
+    xTaskCreate(update_display, "update sh1106 display", 2048, NULL, 5, NULL);
+    xTaskCreate(draw_finger_status, "draw finger status", 2048, NULL, 6, NULL);
 }
